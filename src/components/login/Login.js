d9dc351770ba21c53f3568185323de7410b10adb
DanielTorres-github <85518794+DanielTorres-github@users.noreply.github.com>
2021-06-17 15:33:34 -0300

    Update Login.js

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,19 +1,45 @@
-const Login = () => {
-    return (
-        <form className="container">
-            <div class="form-group">
-                <label for="email">Email address:</label>
-                <input type="email" class="form-control" placeholder="Enter email" id="email" />
-            </div>
-            <div class="form-group">
-                <label for="pwd">Password:</label>
+import React, { Component } from 'react';
 
-                <input type="password" class="form-control" placeholder="Enter password" id="pwd"></input>
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            email:"",
+            pass:""
+         }
+    }
 
-            </div>
-            <button type="submit" class="btn btn-dark">Submit</button>
-        </form>
-    );
+    saveEmail(event){
+        this.setState({
+            email: event.target.value
+        });
+    }
+
+    savePass(event){
+        this.setState({
+            pass: event.target.value
+        });
+    }
+
+
+    render() { 
+        console.log(this.state.email);
+        return (
+            <form className="container">
+                <div class="form-group">
+                    <label for="email">Email address:</label>
+                    <input type="email" class="form-control" placeholder="Enter email" id="email" onChange={(event)=>this.saveEmail(event)}/>
+                </div>
+                <div class="form-group">
+                    <label for="pwd">Password:</label>
+    
+                    <input type="password" class="form-control" placeholder="Enter password" id="pwd" onChange={(event)=>this.savePass(event)}></input>
+    
+                </div>
+                <button type="submit" class="btn btn-dark">Submit</button>
+            </form>
+        );
+    }
 }
-
+ 
 export default Login;
