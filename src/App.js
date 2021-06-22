267c6e7fa3c6f55ee977d976b2529de2c8231779
DanielTorres-github <85518794+DanielTorres-github@users.noreply.github.com>
2021-06-22 20:40:04 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import Nav from "./components/nav/Nav"
 import Login from "./components/login/Login"
 import Footer from "./components/footer/Footer"
 import "./App.css"
+import Home from "./components/home/Home"
+import { Route } from "react-router"
+import { HashRouter } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <Nav></Nav>
-      <Login></Login>
-      <Footer></Footer>
-    </div>
+    <HashRouter>
+      <div className="App">
+        <Nav></Nav>
+        <Route path={["/home", "/"]} component={Home}></Route>
+        <Route path="/login" component={Login}></Route>
+        <Footer></Footer>
+      </div>
+    </HashRouter>
   );
+
 }
 export default App;
