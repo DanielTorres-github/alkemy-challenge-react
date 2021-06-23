de53c02ca664a9f0e0c7acbfe403b93436946716
DanielTorres-github <85518794+DanielTorres-github@users.noreply.github.com>
2021-06-23 18:24:51 -0300

    Nueva ficha de personaje y cambios de estilo en Card

--- src/components/home/card/Card.js
+++ src/components/home/card/Card.js
@@ -19,8 +19,13 @@ class Card extends Component {
                     <li>Velocidad: {this.props.superheroe.powerstats.speed}</li>                    
                     <li>Resistencia: {this.props.superheroe.powerstats.durability}</li>                    
                     <li>Poder: {this.props.superheroe.powerstats.power}</li>                    
-                    <li>Cambate: {this.props.superheroe.powerstats.combat}</li>                    
+                    <li>Combate: {this.props.superheroe.powerstats.combat}</li>                    
                 </ul>
+
+            </div>
+            <div class="row">
+            <button type="button" class="btn btn-info col-sm-2 offset-sm-8">info</button>
+            <button type="button" class="btn btn-danger col-sm-2"> 🗑️ </button>
             </div>
         </div>
          );
