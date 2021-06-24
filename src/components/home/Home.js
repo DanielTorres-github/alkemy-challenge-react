da04e4c8488f17b072de9eb73a38090133e46d11
DanielTorres-github <85518794+DanielTorres-github@users.noreply.github.com>
2021-06-24 19:28:54 -0300

    Nuevos heroes agregados

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -116,7 +116,109 @@ class Home extends Component {
         "image": {
             "url": "https://www.superherodb.com/pictures2/portraits/10/100/835.jpg"
         }
+    },
+    {
+      "response": "success",
+      "id": "316",
+      "name": "Hawkman",
+      "powerstats": {
+          "intelligence": "null",
+          "strength": "66",
+          "speed": "null",
+          "durability": "null",
+          "power": "null",
+          "combat": "null"
+      },
+      "biography": {
+          "full-name": "Carter Hall",
+          "alter-egos": "No alter egos found.",
+          "aliases": [
+              "-"
+          ],
+          "place-of-birth": "-",
+          "first-appearance": "FLASH COMICS #1 (January 1940)",
+          "publisher": "DC Comics",
+          "alignment": "good"
+      },
+      "appearance": {
+          "gender": "Male",
+          "race": "null",
+          "height": [
+              "6'1",
+              "185 cm"
+          ],
+          "weight": [
+              "195 lb",
+              "88 kg"
+          ],
+          "eye-color": "Blue",
+          "hair-color": "Brown"
+      },
+      "work": {
+          "occupation": "Adventurer",
+          "base": "-"
+      },
+      "connections": {
+          "group-affiliation": "Justice Society of America, Justice League of America, All-Star Squadron",
+          "relatives": "Shiera Sanders Hall (wife, deceased; soul currently inhabiting the body of Kendra Saunders), Hector Hall (son, deceased), Daniel Hall (grandson), Lyta Trevor Hall (daughter-in-law, deceased)"
+      },
+      "image": {
+          "url": "https://www.superherodb.com/pictures2/portraits/10/100/839.jpg"
+      }
+  },
+  {
+    "response": "success",
+    "id": "423",
+    "name": "Magneto",
+    "powerstats": {
+        "intelligence": "88",
+        "strength": "80",
+        "speed": "27",
+        "durability": "84",
+        "power": "91",
+        "combat": "80"
+    },
+    "biography": {
+        "full-name": "Erik Magnus Lensherr",
+        "alter-egos": "No alter egos found.",
+        "aliases": [
+            "The Creator",
+            "Erik Magnus Lehnsherr",
+            "Michael Xavier",
+            "White Pilgrim",
+            "Erik the RedThe Master of Magnetism"
+        ],
+        "place-of-birth": "Unrevealed, probably somewhere in Northern Europe",
+        "first-appearance": "X-MEN Vol. 1 #1",
+        "publisher": "Marvel Comics",
+        "alignment": "bad"
+    },
+    "appearance": {
+        "gender": "Male",
+        "race": "Mutant",
+        "height": [
+            "6'2",
+            "188 cm"
+        ],
+        "weight": [
+            "190 lb",
+            "86 kg"
+        ],
+        "eye-color": "Grey",
+        "hair-color": "White"
+    },
+    "work": {
+        "occupation": "Currently Revolutionary and Conqueror, formerly Volunteer Orderly, Secret Agent",
+        "base": "Currently unrevealed, formerly Asteroid M; various hidden bases on Earth including one in Antarctica; an island in the Bermuda Triangle; Professor Xavier's School for Gifted Youngsters, Salem Center, Westchester County, New York"
+    },
+    "connections": {
+        "group-affiliation": "Currently None, formerly Brotherhood of Evil Mutants I and II, X-Men, teacher of the New Mutants, Lords Cardinal of the Hellfire Club, leader of the Acolytes",
+        "relatives": "Magda (wife, deceased), Anya (daughter, deceased), Pietro Maximoff (Quicksilver, son), Wanda Maximoff (Scarlet Witch, daughter), Lorna Dane (Polaris, daughter), Joseph (clone, deceased), Luna (granddaughter)"
+    },
+    "image": {
+        "url": "https://www.superherodb.com/pictures2/portraits/10/100/12.jpg"
     }
+}
       ],
     }
   }
@@ -126,6 +228,8 @@ class Home extends Component {
         <h1 className="col-sm-12">Home SuperHeroes</h1>
         <Card superheroe={this.state.superheroe[0]}></Card>
         <Card superheroe={this.state.superheroe[1]}></Card>
+        <Card superheroe={this.state.superheroe[2]}></Card>
+        <Card superheroe={this.state.superheroe[3]}></Card>
       </div>
 
     );
